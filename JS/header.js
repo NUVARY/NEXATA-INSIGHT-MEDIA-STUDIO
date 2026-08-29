4399e212f715f646b4d0bd2fa84a7720af3b4634
Dennis Moises Palma Juarez <diamantesdmpj@gmail.com>
2026-08-28 23:39:40 -0600

    actaulizacion

--- JS/header.js
+++ JS/header.js
@@ -1,4 +1,4 @@
-fetch('/COMPONENTES/header-global.html')
+fetch('COMPONENTES/header-global.html')
   .then(response => response.text())
   .then(data => {
     document.getElementById('header-global').innerHTML = data;
